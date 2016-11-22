909452dc02ebe152b65acab1b6a77aa68c851d55
Briana Slater <Slater_Briana@bah.com>
2016-11-22 16:47:36 -0500

    Update food.js

--- food.js
+++ food.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 const express = require('express');
@@ -7,52 +8,63 @@ const app = express();
 const sys = require('sys')
 const exec = require('child_process').exec;
 
-app.get('/food', function (req, res) {
-  
-  console.log("In the food");
-  var client = new pg.Client();
-  client.connect(function (err) {
-    if (err) {
-      console.log(err);
-      res.status(404).json({message: "There has been an error connecting to the database."});
-    }
-    else {
-      client.query('SELECT * FROM food', function(err, result) {
-        if (err) {
-          console.log(err);
-          res.status(500).json({message: err});
-        }
-        else {
-          console.log(result.rows);
-          res.status(200).json(result.rows);
-        }
-      });
-    }
-  });
-});
+module.exports= {
+  app.get: function('/food', function (req, res){
+    //app.get('/food', function (req, res) {
+    
+    console.log("In the food");
+    var client = new pg.Client();
+    client.connect(function (err) {
+      if (err) {
+        console.log(err);
+        res.status(404).json({message: "There has been an error connecting to the database."});
+      }
+      else {
+        client.query('SELECT * FROM food', function(err, result) {
+          if (err) {
+            console.log(err);
+            res.status(500).json({message: err});
+          }
+          else {
+            console.log(result.rows);
+            res.status(200).json(result.rows);
+          }
+        });
+      }
+    });
+  //});
 
-app.post('/food', function (req, res) {
-  console.log('Creaing a new wine');
-  var client = new pg.Client();
-  client.connect(function (err) {
-    if (err) {
-      console.log(err);
-      res.status(404).json({message: "There has been an error connecting to databse."});
-    }
-    else {
-      client.query('INSERT INTO food (name, author, rating, category) VALUES ($1, $2, $3,$4)', [req.param('name'), req.param('author'), req.param('rating'), req.param('category')], function(err, result) {
-        if (err) {
-          console.log(err);
-          res.status(500).json({message: "There was problem adding a new record to the database."});
-        }
-        else {
-          console.log(result);
-          res.status(201).json(result);
-        }
-      });
-    }
-  })
-});
+  } //app.get
+
+  app.post: function(/food, function (req, res){
+    //app.post('/food', function (req, res) {
+    console.log('Creaing a new food');
+    var client = new pg.Client();
+    client.connect(function (err) {
+      if (err) {
+        console.log(err);
+        res.status(404).json({message: "There has been an error connecting to databse."});
+      }
+      else {
+        client.query('INSERT INTO food (name, author, rating, category) VALUES ($1, $2, $3,$4)', [req.param('name'), req.param('author'), req.param('rating'), req.param('category')], function(err, result) {
+          if (err) {
+            console.log(err);
+            res.status(500).json({message: "There was problem adding a new record to the database."});
+          }
+          else {
+            console.log(result);
+            res.status(201).json(result);
+          }
+        });
+      }
+      })
+    //});
+
+  } //app.post
+
+}//module.exports
+
+
 
 var child = exec("./node_modules/.bin/pg-migrate up", function (error, stdout, stderr) {
   sys.print('stdout: ' + stdout);
