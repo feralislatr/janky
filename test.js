a2bd0f6fdef200b32d7454c6d771723deeba5832
Briana Slater <Slater_Briana@bah.com>
2016-11-28 13:35:34 -0500

    Update test.js

--- test.js
+++ test.js
@@ -8,26 +8,27 @@ var sinon = require('sinon');
 var mock;
 
 mock = sinon.mock(require('./food'));
-var queryString = 'SELECT * FROM food'
-var queryParams = ""
-mock.expects('query').with(queryString, queryParams).yields(null, rows);
+//var queryString = 'SELECT * FROM food'
+//var queryParams = ""
+//mock.expects('query').with(queryString, queryParams).yields(null, rows);
 
 
 
 
-// test('Correct result returned', function (t) {
-//   request(mock) //app
-//     .get('./food')
-//     //.expect('Content-Type', /json/)
-//     .expect(200)
-//     .end(function (err, res) {
-//       var expectedResponse = 200;
+test('Correct result returned', function (t) {
+  request(mock) //app
+    .get('/food')
+    //.expect('Content-Type', /json/)
+    .expect(200)
+    .end(function (err, res) {
+      var expectedResponse = 200;
 
-//       t.error(err, 'No error');
-//       t.same(res.body, expectedResponse, 'Everything works');
-//       t.end();
-//     });
-// });
+      t.error(err, 'No error');
+      t.same(res.body, expectedResponse, 'Everything works');
+      t.end();
+      mock.verify()
+    });
+});
 
-mock.verify()
+
 mock.restore()
