9c2ea7bbeee1a63abe5b8421fbd38719c6072d8e
Briana Slater <Slater_Briana@bah.com>
2016-11-28 09:03:49 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,73 +1,26 @@
-//Unit Test setup
-var assert = require('assert');
+'use strict';
+
+var test = require('tape');
+var request = require('supertest');
+var app = require('food.js');
+///
 var sinon = require('sinon');
-var PassThrough = require('stream').PassThrough;
-//var PassThrough = require('passthrough');
-var http = require('http');
+var mock;
+mock = sinon.mock(require('./food'));
+//mock.expects('query').with(queryString, queryParams).yields(null, rows);
+///
+mock.verify()
+mock.restore()
+test('Correct users returned', function (t) {
+  request(app)
+    .get('./food')
+    //.expect('Content-Type', /json/)
+    .expect(200)
+    .end(function (err, res) {
+      var expectedResponse = 200;
 
-
-var api = require('./food');
-
-describe('api', function() {
-	beforeEach(function() {
-		this.request = sinon.stub(http, 'request');
-	});
- 
-	afterEach(function() {
-		http.request.restore();
-	});
- 
-  	//Test GET request
-	it('should convert get result to object', function(done) {
-  	var expected = { hello: 'world' };
-  	var response = new PassThrough();
-  	response.write(JSON.stringify(expected));
-  	response.end();
-   
-  	var request = new PassThrough();
-   
-  	this.request.callsArgWith(1, response)
-  	            .returns(request);
-   
-  	api.get(function(err, result) {
-  		assert.deepEqual(result, expected);
-  		done();
-  	});
-  });
-  
-  //Test POST request
-  it('should send post params in request body', function() {
-  	var params = '/food';
-  	var expected = JSON.stringify(params);
-   
-  	var request = new PassThrough();
-  	var write = sinon.spy(request, 'write');
-   
-  	this.request.returns(request);
-   
-  	api.post(params, function() { });
-   
-  	assert(write.withArgs(expected).calledOnce);
-  });
-
-
-  //Test error scenario
-  it('should pass request error to callback', function(done) {
-  	var expected = 'some error';
-  	var request = new PassThrough();
-   
-  	this.request.returns(request);
-   
-  	api.get(function(err) {
-  		assert.equal(err, expected);
-  		done();
-  	});
-   
-	  request.emit('error', expected);
-  });
-
- 
-  
+      t.error(err, 'No error');
+      t.same(res.body, expectedResponse, 'Everything works');
+      t.end();
+    });
 });
-
-//res.status(200).json(result.rows);
