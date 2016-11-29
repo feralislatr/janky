d94cdfc180b15be832b6f58540fbc813b7e136b0
feralislatr <feralislatr@gmail.com>
2016-11-29 14:17:11 -0500

    trying pgtest

--- test.js
+++ test.js
@@ -4,24 +4,10 @@ var test = require('tape');
 var request = require('supertest');
 var app = require('./food');
 ///
+var pgtest = require('pgtest');
 var sinon = require('sinon');
 var db = sinon.mock(app);
 
-var queryString = 'SELECT * FROM food'
-var queryParams = ""
-//db.expects('get').withArgs(queryString, queryParams).yields(null, "");
-
-//sinon.stub(db.query, "get");
-db.expects('get');
-
-
-
-//var request = require('request');
-// request(mock, function (error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     console.log("hi this work") // Print the web page.
-//   }
-// })
 
 //first attempt get test
 // test('Get food', function (assert) {
@@ -38,28 +24,45 @@ db.expects('get');
      
 //     });
 // });
+var expect = pgtest.expect('SELECT * FROM food').returning(null, [
+    [ 'beans', 'raplh', '10', 'protein' ],
+	[ 'green', 'jake', '9', 'veg' ],
+    [ 'potatoes', 'tonka', '8', 'starch' ],
+    [ 'tomatoes', 'rocko' , '7','fruit' ]
 
+]);
+
+var connect = pgtest.connect('food', function (err, client, done) {
+    client.query('SELECT * FROM food', function (err, data) {
+        console.log(data);
+        done();
+    });
+});
 
 //second attempt get test
-// test('GET /food', function (assert) {
-// 	console.log(app.address); //undefined; app.address() is not a function
-//   request(db.app) //app.address undefined when using db.app; not a function with just db
-//     .get('/food')
-//     .expect(200)
-//     .expect('Content-Type', /json/)
-//     .end(function (err, res) {
-//       var expectedThings = [
-//       {food: "", name: "", author: "", rating: ""}
-//       ];
-//       var actualThings = res.body;
+//mock with pgtest in here
+test('GET /food', function (assert) {
+	//console.log(app.address); //undefined; app.address() is not a function
+  request(expect) //app.address undefined when using db.app; not a function with just db
+    .get('/food')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end(function (err, res) {
+      var expectedThings = [
+      {name: "beans", author: "ralph", rating: "10", category: "protein"},
+      {name: "greens", author: "jake", rating: "9", category: "veg"},
+      {name: "potatoes", author: "tonka", rating: "8", category: "starch"},
+      {name: "tomatoes", author: "rocko", rating: "7", category: "fruit"}
+
+      ];
+      var actualThings = res.body;
  
-//       assert.error(err, 'No error');
-//       assert.same(actualThings, expectedThings, 'Retrieve list of food');
-//       assert.end();
-//     });
-// });
+      assert.error(err, 'No error');
+      assert.same(actualThings, expectedThings, 'Retrieve list of food');
+      assert.end();
+    });
+});
 
 //actually gets 404 and returns "There has been an error connecting to the database"
 
-db.verify()
-db.restore()
+pgtest.check();
