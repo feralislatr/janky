ec6b0e205c9673bb80139731389095e078a96efb
feralislatr <feralislatr@gmail.com>
2016-12-07 15:08:56 -0500

    the current 'tests' work

--- test.js
+++ test.js
@@ -5,29 +5,89 @@ var asserts = require('assert');
 var equals = require('array-equal')
 
 
-pgtest.expect('SELECT * FROM food').returning(null, [
+var queries = [
+	'SELECT * FROM food','INSERT INTO food VALUES "potatoes", "tonka", "8", "starch"', 'DELETE FROM food WHERE name="beans"']
+//SELECT
+pgtest.expect(queries[0]).returning(null, [
     [ 'beans', 'raplh', '10', 'protein' ],
 	[ 'greens', 'jake', '9', 'veg' ]
 ]);
 
+//expected result
 var testdata = {rows:[
 	[ 'beans', 'raplh', '10', 'protein' ],
     [ 'greens', 'jake', '9', 'veg' ] 
    ]};
 
 pgtest.connect('food', function (err, client, done){
-	client.query('SELECT * FROM food', function (err, data){
+	client.query(queries[0], function (err, data){
 		asserts(equals(data, testdata));
 		    if (err){
 		    	done(err);
 		    } else{
-		    	console.log("hi i worked; here are data");
-		    	console.log(data);
+		    	console.log("Select test passed", data);
 		       	done();
 		   	}
 		        
 	 });
 });
 
+pgtest.check();
+
+pgtest.reset();
+
+//INSERT
+pgtest.expect(queries[1] ).returning(null, [
+    [ 'beans', 'raplh', '10', 'protein' ],
+	[ 'greens', 'jake', '9', 'veg' ],
+	[ "potatoes", "tonka", "8", "starch" ]
+]);
+
+testdata = {rows:[
+	[ 'beans', 'raplh', '10', 'protein' ],
+    [ 'greens', 'jake', '9', 'veg' ],
+    [ "potatoes", "tonka", "8", "starch" ]
+   ]};
+
+pgtest.connect('food', function (err, client, done){
+	client.query(queries[1], function (err, data){
+		asserts(equals(data, testdata));
+		    if (err){
+		    	done(err);
+		    } else{
+		    	console.log("Insert test passed", data);
+		       	done();
+		   	}
+		        
+	 });
+});
 
 pgtest.check();
+
+pgtest.reset();
+
+//DELETE
+pgtest.expect(queries[2]).returning(null, [
+	[ 'greens', 'jake', '9', 'veg' ]
+]);
+
+//expected result
+testdata = {rows:[
+    [ 'greens', 'jake', '9', 'veg' ] 
+   ]};
+
+pgtest.connect('food', function (err, client, done){
+	client.query(queries[2], function (err, data){
+		asserts(equals(data, testdata));
+		    if (err){
+		    	done(err);
+		    } else{
+		    	console.log("Delete test passed", data);
+		       	done();
+		   	}
+		        
+	 });
+});
+
+pgtest.check();
+
